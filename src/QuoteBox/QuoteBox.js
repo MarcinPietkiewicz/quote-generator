52a82356272a8ce1b68dba3cbe4fb3ccd68236b2
Marcin <marcin.p.dev@gmail.com>
2022-09-16 08:48:17 +0200

    QuoteBox state + preliminary fetch for quotes

--- src/QuoteBox/QuoteBox.js
+++ src/QuoteBox/QuoteBox.js
@@ -1,12 +1,35 @@
 import React from "react";
 // import ReactDOM from 'react-dom/client';
-import './QuoteBox.css';
+import "./QuoteBox.css";
 
 class QuoteBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { quotes: [] };
+    this.getQuotes = this.getQuotes.bind(this);
+  }
+
+  componentDidMount() {
+    const quotes = this.getQuotes()
+    this.setState(
+      {quotes: quotes}
+    )
+  }
+
+  getQuotes = () => {
+    fetch('../quotes.json')
+    .then(function(response){console.log(response)
+    return response.json();})
+    .then(function(myJson) {
+      console.log(myJson);
+    })
+    .catch(err => console.log(err))
+  }
+
   render() {
     return (
       <div id="quote-box">
-        <div id="text">quote text</div>
+        <div id="text">{this.state.quotes ?? 'no quotes loaded'}</div>
         <div id="author">author</div>
         <button id="new-quote">NEW QUOTE</button>
         <button id="tweet-quote">tweet-logo</button>

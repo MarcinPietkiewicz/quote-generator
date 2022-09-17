f40b2e0a04b4700cc5168791bda7a0297c6a851e
Marcin <marcin.p.dev@gmail.com>
2022-09-18 00:15:00 +0200

    Component quote-box loads first quote from json

--- src/QuoteBox/QuoteBox.js
+++ src/QuoteBox/QuoteBox.js
@@ -5,8 +5,8 @@ import "./QuoteBox.css";
 class QuoteBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {quotes: [] };
-    this.fetchQuotes = this.fetchQuotes.bind(this);
+    this.state = { quotes: []};
+    // this.fetchQuotes = this.fetchQuotes.bind(this);
   }
 
 
@@ -15,25 +15,25 @@ class QuoteBox extends React.Component {
   }
 
   fetchQuotes() {
-    this.setState(() => {
-      fetch("http://localhost:8000/db")
+      fetch("http://localhost:8000/quotes")
+      // json-server --watch quotes.json --port 8000
       // fetch("https://farmerolaf.com/jsons/quotes.json") // this is working
-      .then(resolve => resolve.json())
+      .then(response => response.json())
       .then(result => {
-        return this.setState({
-        quotes: result
-      })}).catch(console.log);
+        // console.log(result);
+        this.setState({quotes: result})
+      }).catch(console.log);
 
-  })
   }
 
   render() {
     return (
       <div id="quote-box">
-        <div id="text">{this.state.quotes[0] ?? 'no quotes loaded'}</div>
-        <div id="author">author</div>
+        <div id="text">{this.state.quotes[0]?.quote ?? 'no quotes loaded'}</div>
+        <div id="author">{this.state.quotes[0]?.author ?? 'no author loaded'}</div>
         <button id="new-quote">NEW QUOTE</button>
         <button id="tweet-quote">tweet-logo</button>
+        {/* {console.log('state in quotes ->'+this.state.quotes[1]?.author ?? 'nope')} */}
       </div>
     );
   }

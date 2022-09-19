6d8e4ce3709b7d9fcdfa77c503b6f792da5e9c33
Marcin <marcin.p.dev@gmail.com>
2022-09-19 08:29:33 +0200

    Added error handling for not resolved fetch and 404 resolve

--- src/QuoteBox/QuoteBox.js
+++ src/QuoteBox/QuoteBox.js
@@ -16,25 +16,29 @@ class QuoteBox extends React.Component {
   }
 
   fetchQuotes() {
-      fetch("http://localhost:8000/db") //json-server dev mock-up // json-server --watch quotes.json --port 8000
+      fetch("http://localhost:8000/dbj") //json-server dev mock-up // json-server --watch quotes.json --port 8000
       // fetch("https://farmerolaf.com/jsons/quotes.json") // swap to this in production
       .then(response => response.json())
       .then(result => {
         this.setState({quotes: result.quotes});
-      }).catch(console.log);
+      }).catch(function (err) {alert ('Fetch error: ', err)});
 
   }
   // returns random quote number from quotes array if exists otherwise 0
   rndQuoteNum() {
-    return  Math.floor(Math.random() * (this.state.quotes.length + 1)) ?? 0; 
+    if (this.state.quotes !== undefined){
+    return  Math.floor(Math.random() * (this.state.quotes.length + 1))}
+    else {
+      return 0;
+    }; 
   }
 
   render() {
 
     return (
       <div id="quote-box">
-        <div id="text">{this.state.quotes[this.rndQuoteNum()]?.quote ?? "loading quote..."}</div>
-        <div id="author">{this.state.quotes[this.rndQuoteNum()]?.author ?? "..."}</div>
+        <div id="text">{(this.state.quotes !== undefined) ? (this.state.quotes[this.rndQuoteNum()]?.quote ?? "loading quote...") : "Error fetching quotes"}</div>
+        <div id="author">{(this.state.quotes !== undefined) ? (this.state.quotes[this.rndQuoteNum()]?.author ?? "...") : "..."}</div>
         <button id="new-quote">NEW QUOTE</button>
         <button id="tweet-quote">tweet-logo</button>
       </div>

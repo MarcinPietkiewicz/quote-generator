fb5dbb376aad5fe3c4bd91e0b945283439e0938e
Marcin <marcin.p.dev@gmail.com>
2022-09-20 08:10:12 +0200

    Fixed rendering random quote after first fetch of json

--- src/QuoteBox/QuoteBox.js
+++ src/QuoteBox/QuoteBox.js
@@ -4,7 +4,7 @@ import "./QuoteBox.css";
 class QuoteBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { randomNumber: null, quotes: [] };
+    this.state = { rnd: 0, quotes: [] };
     this.fetchQuotes = this.fetchQuotes.bind(this);
     this.rndQuoteNum = this.rndQuoteNum.bind(this);
     this.handleClick = this.handleClick.bind(this);
@@ -19,37 +19,33 @@ class QuoteBox extends React.Component {
       // fetch("https://farmerolaf.com/jsons/quotes.json") // swap to this in production
       .then((response) => response.json())
       .then((result) => {
-        let rnd = this.rndQuoteNum();
-        this.setState({ random: rnd, quotes: result.quotes });
+        let num = this.rndQuoteNum(result.quotes.length ?? 0);
+        this.setState({ rnd: num, quotes: result.quotes });
       })
       .catch((err) => console.log("Fetch error: " + err));
   }
 
-  rndQuoteNum() {
+  rndQuoteNum(len) {
     if (this.state.quotes !== undefined) {
-      return Math.floor(Math.random() * (this.state.quotes.length + 1));
+      return Math.floor(Math.random() * (len + 1));
     } else {
       return 0;
     }
   }
 
   handleClick() {
-    const r = this.rndQuoteNum();
-    return this.setState({ random: r });
+    const r = this.rndQuoteNum(this.state.quotes.length);
+    return this.setState({ rnd: r });
   }
 
   render() {
     return (
       <div id="quote-box">
         <div id="text">
-          {this.state.quotes !== undefined
-            ? this.state.quotes[this.rndQuoteNum()]?.quote ?? "loading quote..."
-            : "Error fetching quotes"}
+          {this.state.quotes !== undefined ? this.state.quotes[this.state.rnd]?.quote ?? "loading quote..." : "Error fetching quotes"}
         </div>
         <div id="author">
-          {this.state.quotes !== undefined
-            ? this.state.quotes[this.rndQuoteNum()]?.author ?? ""
-            : ""}
+          {this.state.quotes !== undefined ? this.state.quotes[this.state.rnd]?.author ?? "" : ""}
         </div>
         <button id="tweet-quote">tweet-logo</button>
         <button id="new-quote" onClick={this.handleClick}>
